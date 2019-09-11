2f6dde461d7d51ffe3fb013467039c6002cf95a9
MegaKreja <kreja95ue@gmail.com>
2019-09-11 13:59:11 +0200

    finished contact page

--- themes/sportstring/contact-page.php
+++ themes/sportstring/contact-page.php
@@ -1,23 +1,25 @@
 <?php
 /*Template Name: Contact Us*/
 get_header();?>
-<div class="container">
-  <?php  
-  
-  while ( have_posts() ) :
-				the_post();
+<div class="contact row">
+  <div class="container">
+    <?php  
+    
+    while ( have_posts() ) :
+          the_post();
 
-				the_content();
+          the_content();
 
-			endwhile; // End of the loop.
-  
-  ?>
+        endwhile; // End of the loop.
+    
+    ?>
+  </div>
 </div>
-<div class="nearfooter-contact resources-nearfooter row">
+<!-- <div class="nearfooter-contact resources-nearfooter row">
   <h1>Contact us today to see how we can help!</h1>
   <button type="button" class="btn green">CALL NOW</button>
   <button type="button" class="btn">EMAIL</button>
-</div>
+</div> -->
 <?php
 get_footer();
 ?>
--- themes/sportstring/resources-page.php
+++ themes/sportstring/resources-page.php
@@ -24,28 +24,30 @@ get_header();?>
               array('image' => false, 'video' => false)
             );
             for($i=0; $i < count($posts); $i++): ?>
-              <div class="post">
-                <h1>Heading Text</h1>
-                <div class="time">
-                  <p class="likeButton">Physiotherapy</p>
-                  <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 251, 'full', false )[0]  ?>" alt="">
-                  <p>01.01.2018</p>
-                </div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae voluptatibus tempore harum ut excepturi sint repudiandae voluptatem voluptas mollitia. Laboriosam, consequatur! Eos sequi neque, iusto maiores ipsam cupiditate est molestias.</p>
-                <div class="social-info row">
-                  <button type="button" class="btn col-4">READ MORE</button>
-                  <div class="col-4"></div>
-                  <div class="col-4">
-                    <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 252, 'full', false )[0]  ?>" alt="">
-                    <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 253, 'full', false )[0]  ?>" alt="">
-                    <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 254, 'full', false )[0]  ?>" alt="">
-                    <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 255, 'full', false )[0]  ?>" alt="">
-                    <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 256, 'full', false )[0]  ?>" alt="">
-                  </div>
-                </div>
-             </div>
-          <?php endfor; ?>
+          <div class="post">
+            <h1>Heading Text</h1>
+            <div class="time">
+              <p class="likeButton">Physiotherapy</p>
+              <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 251, 'full', false )[0]  ?>" alt="">
+              <p>01.01.2018</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae voluptatibus tempore harum ut excepturi
+              sint repudiandae voluptatem voluptas mollitia. Laboriosam, consequatur! Eos sequi neque, iusto maiores
+              ipsam cupiditate est molestias.</p>
+            <div class="social-info row">
+              <button type="button" class="btn col-4">READ MORE</button>
+              <div class="col-4"></div>
+              <div class="col-4">
+                <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 252, 'full', false )[0]  ?>" alt="">
+                <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 253, 'full', false )[0]  ?>" alt="">
+                <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 254, 'full', false )[0]  ?>" alt="">
+                <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 255, 'full', false )[0]  ?>" alt="">
+                <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 256, 'full', false )[0]  ?>" alt="">
+              </div>
+            </div>
           </div>
+          <?php endfor; ?>
+        </div>
       </div>
       <div class="col-lg-4 right d-none d-lg-block">
         <h1>Get in Touch</h1>
@@ -68,26 +70,26 @@ get_header();?>
         <ul class="featuredPosts">
           <?php
             for($i=1; $i <= 6; $i++): ?>
-              <li class="row">
-                <div class="col-4 post-image">
-                  <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 261, 'full', false )[0]  ?>" alt="">
-                  <p class="centered">Photo #<?=$i ?></p>
-                </div>
-                <div class="col-8 post-text">
-                  <h2>Heading text #<?=$i ?></h2>
-                  <p class="likeButton">Physiotherapy</p>
-                  <div class="time">
-                    <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 251, 'full', false )[0]  ?>" alt="">
-                    <p>01.01.2018</p>
-                  </div>
-                </div>
-              </li>
+          <li class="row">
+            <div class="col-4 post-image">
+              <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 261, 'full', false )[0]  ?>" alt="">
+              <p class="centered">Photo #<?=$i ?></p>
+            </div>
+            <div class="col-8 post-text">
+              <h2>Heading text #<?=$i ?></h2>
+              <p class="likeButton">Physiotherapy</p>
+              <div class="time">
+                <img class="img-fluid" src="<?php echo wp_get_attachment_image_src( 251, 'full', false )[0]  ?>" alt="">
+                <p>01.01.2018</p>
+              </div>
+            </div>
+          </li>
           <?php endfor; ?>
         </ul>
       </div>
     </div>
   </div>
-  
+
 </div>
 <div class="nearfooter-contact resources-nearfooter row">
   <h1>Contact us today to see how we can help!</h1>
